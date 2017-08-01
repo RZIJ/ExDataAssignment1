0c3c774e96a3496147bcfca00d972855b4fe2b80
Rick <r.van.zijl@rijkzwaan.nl>
2017-08-01 13:09:17 +0200

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -10,12 +10,14 @@ df <- read.table("household_power_consumption.txt",
                  ##                  "numeric", "numeric", "numeric")
 )
 df <- df[df$Date == "1/2/2007" |df$Date == "2/2/2007" ,]
+png("Plot4.png")
 par(mfrow=c(2,2))
 plot(df$datetime, df$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
-plot(df$datetime, df$Voltage, type = "l", xlab = "", ylab = "Voltage")
-plot(df$datetime, df$Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = "datetime")
+plot(df$datetime, df$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(df$datetime, df$Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = "")
 lines(df$datetime, df$Sub_metering_2, col = "red")
 lines(df$datetime, df$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), 
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), bty = "n", 
        col = c("black", "red", "blue"), lty = "solid")
 plot(df$datetime, df$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power")
+dev.off()
